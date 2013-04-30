0fbd94cb44dec9380c1f10828a9bffcfe55faf8c
unknown <Administrator@HP-005.l410.cwa.ac.uk>
2013-04-30 10:10:23 +0100

    Updated blank Ant build file from previous merge Added missing chartview class from previous merge

---
+++ netflow/src/cwa/netflow/gui/ChartView.java
@@ -0,0 +1,19 @@
+package cwa.netflow.gui;
+
+import javax.swing.JPanel;
+
+import cwa.netflow.models.IPRangeFilter;
+
+public class ChartView extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3436496944679612402L;
+
+	public void setIPRangeFilter(IPRangeFilter rf) {
+		// TODO: apply an iprange filter to the chart view
+		
+	}
+
+}
